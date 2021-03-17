a1c35d9216003fd4789fb5193fc46231da39ad7b
Ridwan ONIKOYI <onikoyiridwan@gmail.com>
2021-03-17 17:12:34 +0100

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+app.config.performance = true;
+
+app.use(store).use(router).mount('#app');
